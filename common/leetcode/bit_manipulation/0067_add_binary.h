0551e6c5be7ff24312f0f0adc50af271cb90c6c1
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2024-01-08 07:58:30 +0530

    add binary numbers leetcode attempt

--- common/leetcode/bit_manipulation/0067_add_binary.h
+++ common/leetcode/bit_manipulation/0067_add_binary.h
@@ -1,6 +1,7 @@
 #if !defined(add_binary_h)
 
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -11,7 +12,26 @@ class solution {
     {
         string result;
 
+        unsigned int inputA = 0, inputB = 0;
 
+        int bit = 0;
+        for (int i = a.length() - 1, bit = 0; i >= 0; --i, ++bit) {
+            inputA += (pow(2, bit) * (unsigned int)(a[i] - '0'));
+        }
+        for (int i = b.length() - 1, bit = 0; i >= 0; --i, ++bit) {
+            inputB += (pow(2, bit) * (unsigned int)(b[i] - '0'));
+        }
+
+        unsigned int sum = inputA + inputB;
+
+        unsigned int shift = 0;
+        unsigned int n = 2;
+        while(n > 1) {
+            n = sum >> shift;
+            char set = (n & 0x1) ? '1' : '0';
+            result = set + result;
+            shift++;
+        }
 
         return result;
     }
