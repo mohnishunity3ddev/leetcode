2476816bd0d6b9594b7b767950da0a0302859220
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2024-01-18 09:39:22 +0530

    move printRootToItemPath method into binary_tree struct

--- common/containers/tree/binary_tree/binary_tree.h
+++ common/containers/tree/binary_tree/binary_tree.h
@@ -256,6 +256,89 @@ struct binary_tree
         std::cout << "\n";
     }
 
+    void
+    printRootToItemPath(T item)
+    {
+        // post-order
+        stack<binary_tree_node<T> *> s{32};
+        std::vector<T> path;
+
+        bool found = false;
+        auto *curr = root;
+
+        do {
+            while(curr != nullptr) {
+                if(curr->right) {
+                    s.push(curr->right);
+                }
+                s.push(curr);
+                curr = curr->left;
+            }
+
+            curr = s.pop();
+            if(curr->val == item) {
+                found = true;
+                break;
+            }
+
+            if(curr->right && s.peek() == curr->right) {
+                s.pop();
+                s.push(curr);
+                curr = curr->right;
+            }
+            else {
+                curr = nullptr;
+            }
+        } while (!s.isEmpty());
+
+        if(found) {
+            binary_tree_node<T> *prev;
+            bool done = false;
+            while(!done) {
+                // std::cout << curr->val << " <-- ";
+                path.push_back(curr->val);
+                if(curr->right && curr->right == s.peek()) {
+                    s.pop();
+                }
+                prev = curr;
+                if(!s.isEmpty()) {
+                    curr = s.pop();
+                    if(curr->right == prev) {
+                        path.push_back(1);
+                    } else {
+                        path.push_back(-1);
+                    }
+                } else {
+                    curr = nullptr;
+                    done = true;
+                }
+            }
+            if(curr) {
+                path.push_back(curr->val);
+            }
+        } else {
+            std::cout << "could not find the element!\n";
+        }
+
+        if(path.size() > 0) {
+            for(int i = path.size() - 1; i >= 0; i -= 2) {
+                std::cout << "(" << path[i];
+                if(i > 0) {
+                    std::cout << ") --> [";
+                    int dir = path[i - 1];
+                    ASSERT(dir == 1 || dir == -1);
+                    if(dir == -1) {
+                        std::cout << "left";
+                    } else if(dir == 1) {
+                        std::cout << "right";
+                    }
+                    std::cout << "] --> ";
+                }
+            }
+            std::cout << ").\n";
+        }
+    }
+
     static void
     test()
     {
